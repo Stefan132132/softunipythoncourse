cc72d01e2b37bab6a4af14176e1e99a92c04b4d9
Stefan Ivanov <stefanivanov0786@gmail.com>
2023-10-08 10:17:17 +0300

    bonus score excercise

---
+++ bonus_score.py
@@ -0,0 +1,18 @@
+num = int(input())
+
+bonus = 0
+
+if num <= 100:
+    bonus = bonus + 5
+elif num > 1000:
+    bonus = bonus + (0.10 * num)
+else:
+    bonus = bonus + (0.20 * num)
+    
+if num % 2 == 0:
+    bonus = bonus + 1
+elif num % 5 == 0 and num % 2 != 0:
+    bonus = bonus + 2
+
+print(bonus)
+print(num + bonus)
