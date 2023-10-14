5d71cbc745660236acb5a966669ddccf9275c588
Stefan Ivanov <stefanivanov0786@gmail.com>
2023-10-14 12:23:18 +0300

    simple exercise

---
+++ cinema_ticket.py
@@ -0,0 +1,18 @@
+day = input()
+
+prices = {"Monday":12, "Tuesday":12, "Wednesday":14, "Thursday":14, "Friday":12, "Saturday":16, "Sunday":16}
+
+print(prices[day])
+
+
+
+
+
+# ticket_price = 0
+# if day == "Monday" or day == "Tuesday" or day == "Friday":
+#     ticket_price = 12
+# elif day == "Wednesday" or day == "Thursday":
+#     ticket_price = 14
+# elif day == "Saturday" or day == "Sunday":
+#     ticket_price =16
+#print(ticket_price)
